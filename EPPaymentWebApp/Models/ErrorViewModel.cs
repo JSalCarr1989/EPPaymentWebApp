2cc60ada6645e27ad7d5499c7bdb9b6c95f8f542
Jesús Salvador Carrillo Gonzalez <co_jcarillog@axtel.com.mx>
2018-10-22 17:32:51 -0500

    Add project files.

---
+++ EPPaymentWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EPPaymentWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
